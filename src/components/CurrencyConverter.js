8600bc4ca9242ba81c22685be553853c179baa97
galaxy28 <51984459+jyotimishra05@users.noreply.github.com>
2022-11-02 18:30:04 +0530

    Update CurrencyConverter.js

--- src/components/CurrencyConverter.js
+++ src/components/CurrencyConverter.js
@@ -1,9 +1,59 @@
-import React from 'react'
+import React from "react";
 
-export default function CurrencyConverter() {
-    return (
-        <div>
-            
-        </div>
-    )
+function Converter(props) {
+  // const firstData = props.data.filter((rate) => {
+  //   return rate !== props.secondInput;
+  // });
+  // const secData = props.data.filter((rate) => {
+  //   return rate !== props.firstInput;
+  // });
+  return (
+    <div className="conveter_container">
+      <p>
+        {props.fromAmount} {props.firstInput} is eqauls to
+      </p>
+      <h3>
+        {props.toAmount} {props.secondInput}
+      </h3>
+      <div>
+        <select value={props.firstInput} onChange={props.handleFromCurreny}>
+          {props.data
+            .filter((I) => I !== props.secondInput)
+            .map((rate, index) => {
+              return (
+                <option key={index} value={rate}>
+                  {rate}
+                </option>
+              );
+            })}
+        </select>
+        <input
+          type="number"
+          value={props.fromAmount}
+          onChange={props.onMoneyChangeFrom}
+          min="0"
+        />
+      </div>
+      <div>
+        <select value={props.secondInput} onChange={props.handleToCurrency}>
+          {props.data
+            .filter((I) => I !== props.firstInput)
+            .map((rate, index) => {
+              return (
+                <option key={index} value={rate}>
+                  {rate}
+                </option>
+              );
+            })}
+        </select>
+        <input
+          type="number"
+          value={props.toAmount}
+          onChange={props.onMoneyChangeTo}
+          min="0"
+        />
+      </div>
+    </div>
+  );
 }
+export default Converter;
